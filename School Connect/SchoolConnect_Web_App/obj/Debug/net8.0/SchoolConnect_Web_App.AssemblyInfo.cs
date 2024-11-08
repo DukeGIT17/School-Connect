37b998c88ce2af08bdd749f992be834d1b099bf5
Innocent Poti <thembileinnocent16@gmail.com>
2024-11-08 12:44:21 +0200

    Latest Version 08-11-2024

--- School Connect/SchoolConnect_Web_App/obj/Debug/net8.0/SchoolConnect_Web_App.AssemblyInfo.cs
+++ School Connect/SchoolConnect_Web_App/obj/Debug/net8.0/SchoolConnect_Web_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolConnect_Web_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+701e6af1b2bda653fec000b19969229f2e001973")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a59a37fb43e5cf2bfe4ae9194ffdd9c4c0a444")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolConnect_Web_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolConnect_Web_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
